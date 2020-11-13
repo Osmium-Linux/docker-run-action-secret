2c155fc33d748701aa6bd98d24ec5ef33591554e
Eile Kerning <tysayahi@gmail.com>
2020-11-12 21:46:37 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ fi
 
 echo "$INPUT_RUN" | sed -e 's/\\n/;/g' > semicolon_delimited_script
 
-echo "$SUPER_SECRET" && exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --env $SUPER_SECRET='$SUPER_SECRET' --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "`cat semicolon_delimited_script`"
+echo "$SUPER_SECRET" && exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --env SUPER_SECRET="$SUPER_SECRET" --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "`cat semicolon_delimited_script`"
